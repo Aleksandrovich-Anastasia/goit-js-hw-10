a8d186d89ae9d42ceb623a40471d942625fa5392
Aleksandrovich-Anastasia <78806033+Aleksandrovich-Anastasia@users.noreply.github.com>
2025-07-13 11:18:50 +0000

    Deploying to gh-pages from @ Aleksandrovich-Anastasia/goit-js-hw-10@c27a5abc88d446de83e07bea91318e332b159feb 🚀

--- 2-snakbar.js
+++ 2-snakbar.js
@@ -1,10 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                     */import{i as a}from"./assets/vendor-B0WGwTj1.js";const e=document.querySelector(".form");e.addEventListener("submit",s=>{s.preventDefault();const o=Number(e.elements.delay.value),i=e.elements.state.value;c(o,i).then(t=>{a.success({message:`
+import{s as i}from"./assets/sprite-Bj-bcaRk.js";/* empty css                     */import{i as r}from"./assets/vendor-B0WGwTj1.js";const e=document.querySelector(".form");e.addEventListener("submit",s=>{s.preventDefault();const o=Number(e.elements.delay.value),a=e.elements.state.value;c(o,a).then(t=>{r.success({message:`
           <svg class="toast-icon" width="24" height="24">
-            <use xlink:href="./img/sprite.svg#icon-ok"></use>
-          </svg>
-          <span class="toast-text-acc">OK</span><span class="toast-text">Fulfilled promise in ${t}ms</span>`,position:"topRight",timeout:3e3,icon:"",close:!1,class:"custom-toast custom-toast-success"})}).catch(t=>{a.error({message:`
+                <use xlink:href="${i}#icon-ok"></use>
+              </svg>
+              <span class="toast-text-acc">Error</span>
+              <span class="toast-text">Fulfilled promise in ${t}ms</span>`,position:"topRight",timeout:3e3,icon:"",close:!1,class:"custom-toast custom-toast-success"})}).catch(t=>{r.error({message:`
           <svg class="toast-icon" width="24" height="24">
-            <use xlink:href="./img/sprite.svg#icon-error"></use>
-          </svg>
-          <span class="toast-text-acc">Error</span><span class="toast-text">Rejected promise in ${t}ms</span>`,position:"topRight",icon:"",timeout:3e3,close:!1,class:"custom-toast custom-toast-error"})}),e.reset()});function c(s,o){return new Promise((i,t)=>{setTimeout(()=>{o==="fulfilled"?i(s):t(s)},s)})}
+                <use xlink:href="${i}#icon-error"></use>
+              </svg>
+              <span class="toast-text-acc">Error</span>
+              <span class="toast-text">Rejected promise in ${t}ms</span>`,position:"topRight",icon:"",timeout:3e3,close:!1,class:"custom-toast custom-toast-error"})}),e.reset()});function c(s,o){return new Promise((a,t)=>{setTimeout(()=>{o==="fulfilled"?a(s):t(s)},s)})}
 //# sourceMappingURL=2-snakbar.js.map
