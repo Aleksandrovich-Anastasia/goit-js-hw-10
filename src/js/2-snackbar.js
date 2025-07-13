8e5e813e4f1536a02258f85613829b3c52620fb8
Anastasia <aleksandrovich2109@gmail.com>
2025-07-13 13:27:17 +0300

    hw10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,55 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        message: `
+          <svg class="toast-icon" width="24" height="24">
+            <use xlink:href="./img/sprite.svg#icon-ok"></use>
+          </svg>
+          <span class="toast-text-acc">OK</span><span class="toast-text">Fulfilled promise in ${delay}ms</span>`,
+        position: 'topRight',
+        timeout: 3000,
+        icon: '',
+        close: false,
+        class: 'custom-toast custom-toast-success',  
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `
+          <svg class="toast-icon" width="24" height="24">
+            <use xlink:href="./img/sprite.svg#icon-error"></use>
+          </svg>
+          <span class="toast-text-acc">Error</span><span class="toast-text">Rejected promise in ${delay}ms</span>`,
+        position: 'topRight',
+        icon: '',
+        timeout: 3000,
+        close: false,
+        class: 'custom-toast custom-toast-error',  
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
